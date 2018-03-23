67c88c04161e8750b1dd5c31bb8d2339a20362f8
Andrew Kairys <andrewkairys@gmail.com>
2018-03-23 16:37:02 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,12 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backers
 
   def initialize(title)
     @title = title
+    @backers = Array.new
   end
 
+  def add_backer(backer)
+    @backers.push(backer)
+    backer.backed_projects.push(self)
 end
